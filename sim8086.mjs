9f126c698724d094bb79dd5d5f28c59165c9989a
Luke Chinworth <lukechinworth@gmail.com>
2023-03-27 21:41:36 -0500

    wip listing 41

--- sim8086.mjs
+++ sim8086.mjs
@@ -4,6 +4,19 @@ const [, , input_path] = process.argv;
 
 const buffer = readFileSync(input_path);
 
+const rm_op_map = {
+    "100010": "mov",
+    "000000": "add",
+    "001010": "sub",
+    "001110": "cmp"
+};
+
+const immediate_op_map = {
+    "000": "add",
+    "101": "sub",
+    "111": "cmp"
+};
+
 // Top level key is w bit.
 const reg_map = {
     "0": {
@@ -41,9 +54,11 @@ const rm_map = {
 
 let d, // "direction", 1 = reg is dest, 0 = reg is source.
     w, // "word", i.e. 16bit
+    s, // "sign"
     mod, // reg2reg, reg2mem, mem2mem, mem2reg, etc.
     reg, // reg address
     rm, // reg/mem address
+    disp,
     data;
 
 console.log("bits 16");
@@ -51,9 +66,11 @@ console.log("bits 16");
 for (let i = 0; i < buffer.length; i++) {
     let byte = buffer.readUInt8(i);
     let bits = byte.toString(2).padStart(8, "0");
+    let op_code = bits.slice(0, 6);
+    let op = rm_op_map[op_code];
 
-    // register/memory to/from register
-    if (bits.slice(0, 6) === "100010") {
+    // register/memory and register
+    if (op) {
         d = bits[6];
         w = bits[7];
         i++;
@@ -70,9 +87,9 @@ for (let i = 0; i < buffer.length; i++) {
             case "11":
                 const rm_reg_address = reg_map[w][rm];
                 if (d === "1") {
-                    console.log(`mov ${reg_address}, ${rm_reg_address}`);
+                    console.log(`${op} ${reg_address}, ${rm_reg_address}`);
                 } else {
-                    console.log(`mov ${rm_reg_address}, ${reg_address}`);
+                    console.log(`${op} ${rm_reg_address}, ${reg_address}`);
                 }
                 continue;
                 break;
@@ -83,21 +100,79 @@ for (let i = 0; i < buffer.length; i++) {
 
                 if (mod === "01") { // 8bit displacement
                     i++;
-                    data = buffer.readUInt8(i);
-                    mem_address += ` + ${data}`;
+                    disp = buffer.readUInt8(i);
+                    mem_address += ` + ${disp}`;
                 } else if (mod === "10") {// 16bit displacement
                     i++;
-                    data = buffer.readUInt16LE(i);
+                    disp = buffer.readUInt16LE(i);
                     i++;
-                    mem_address += ` + ${data}`;
+                    mem_address += ` + ${disp}`;
                 }
 
                 if (d === "1") {
-                    console.log(`mov ${reg_address}, [${mem_address}]`);
+                    console.log(`${op} ${reg_address}, [${mem_address}]`);
                 } else {
-                    // TODO: last test in listing 39 not printing.
-                    console.log(`mov [${mem_address}], ${reg_address}`);
+                    console.log(`${op} [${mem_address}], ${reg_address}`);
                 }
+                continue;
+                break;
+        }
+    }
+
+    // immedidate and register/memory
+    if (op_code === "100000") {
+        s = bits[6]; // TODO: unused for now.
+        w = bits[7];
+        i++;
+        byte = buffer.readUInt8(i);
+        bits = byte.toString(2).padStart(8, "0");
+        mod = bits.slice(0, 2);
+        op_code = bits.slice(2, 5);
+        op = immediate_op_map[op_code];
+        rm = bits.slice(5, 8);
+
+        switch (mod) {
+            // register mode
+            case "11":
+                const reg_address = reg_map[w][rm];
+                if (w === "1") {
+                    i++;
+                    data = buffer.readInt16LE(i)
+                    i++;
+                } else {
+                    i++;
+                    data = buffer.readUInt8(i);
+                }
+
+                console.log(`${op} ${reg_address}, ${data}`);
+                continue;
+                break;
+            // memory mode
+            default:
+                // TODO: displacement exception for mod = 00 and rm = 110
+                let mem_address = rm_map[rm];
+
+                if (mod === "01") { // 8bit displacement
+                    i++;
+                    disp = buffer.readUInt8(i);
+                    mem_address += ` + ${disp}`;
+                } else if (mod === "10") {// 16bit displacement
+                    i++;
+                    disp = buffer.readUInt16LE(i);
+                    i++;
+                    mem_address += ` + ${disp}`;
+                }
+
+                if (w === "1") {
+                    i++;
+                    data = buffer.readInt16LE(i)
+                    i++;
+                } else {
+                    i++;
+                    data = buffer.readUInt8(i);
+                }
+
+                console.log(`${op} [${mem_address}], ${data}`);
                 continue;
                 break;
         }
@@ -107,12 +182,13 @@ for (let i = 0; i < buffer.length; i++) {
     if (bits.slice(0, 4) === "1011") {
         w = bits[4];
         reg = bits.slice(5, 8);
-        i++;
 
         if (w === "1") {
+            i++;
             data = buffer.readUInt16LE(i)
             i++;
         } else {
+            i++;
             data = buffer.readUInt8(i);
         }
 
